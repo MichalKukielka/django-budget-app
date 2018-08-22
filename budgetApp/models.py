06454fe89d6d5d1b0bdef5837fde3ab0916b74df
Michał Kukiełka <kukielka.michal@gmail.com>
2018-08-22 07:25:39 +0200

    cmt
    cmt

--- budgetApp/models.py
+++ budgetApp/models.py
@@ -13,12 +13,12 @@ class Project(models.Model):
 
 
 class Category(models.Model):
-    poject = models.ForeignKey(Project, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
 
 
 class Expense(models.Model):
-    poject = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='expenses')
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='expenses')
     title = models.CharField(max_length=256)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
