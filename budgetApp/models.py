e1e08eaa647166d41fc8a456655b4654b24ce356
Michał Kukiełka <kukielka.michal@gmail.com>
2018-08-22 14:59:42 +0200

    fc
    fc

--- budgetApp/models.py
+++ budgetApp/models.py
@@ -11,6 +11,23 @@ class Project(models.Model):
         self.slug = slugify(self.name)
         super(Project, self).save(*args, **kwargs)
 
+    @property
+    def budget_left(self):
+
+        expense_list = Expense.objects.filter(project=self)
+        total_expense_amount = 0
+
+        for expense in expense_list:
+            total_expense_amount += expense.amount
+
+        return self.budget - total_expense_amount
+
+    @property
+    def total_transactions(self):
+
+        expense_list = Expense.objects.filter(project=self)
+        return len(expense_list)
+
 
 class Category(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
@@ -18,7 +35,11 @@ class Category(models.Model):
 
 
 class Expense(models.Model):
+
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='expenses')
     title = models.CharField(max_length=256)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-amount',)
